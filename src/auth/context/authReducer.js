182e0a0d19eb3bfaf2f76b8534d96cd23df44f27
Andres <andres.torrez.work@gmail.com>
2023-08-30 16:52:00 -0400

    AuthProvider 05 terminado

---
+++ src/auth/context/authReducer.js
@@ -0,0 +1,30 @@
+import { types } from "../types/types";
+
+// const initialState = {
+//     logged: false
+// }
+
+// state = initialState
+
+export const authReducer = ( state = {}, action ) => {
+    
+    switch ( action.type ) {
+
+        case types.login:
+            return {
+                ...state,
+                logged: true,
+                name: action.payload
+            };
+
+        case types.logout:
+            return {
+                logged: false,
+            };
+
+        default:
+            return state;
+
+    }
+    
+}
