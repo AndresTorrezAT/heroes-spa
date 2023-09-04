a8ac23749112e1c27837e73de09991093bede088
Andres <andres.torrez.work@gmail.com>
2023-09-03 22:53:29 -0400

    test iniciado

---
+++ test/auth/context/authReducer.test.js
@@ -0,0 +1,11 @@
+import { authReducer } from "../../../src/auth/context/authReducer";
+
+
+describe('Pruebas en authReducer', () => {
+
+    test('Debe de retornar el estado por defecto', () => {
+
+        authReducer
+
+    });
+});
