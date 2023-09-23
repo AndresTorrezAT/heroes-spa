3620e77d8b7eb012213bec2fa358e60a346b2333
Andres <andres.torrez.work@gmail.com>
2023-09-23 19:05:29 -0400

    014 iniciando

--- test/heroes/pages/SearchPage.test.jsx
+++ test/heroes/pages/SearchPage.test.jsx
@@ -1,6 +1,6 @@
 const { render, screen } = require("@testing-library/react");
 const { MemoryRouter } = require("react-router-dom");
-// const { SearchPage } = require("../../../src/heroes/pages/SearchPage");
+const { SearchPage } = require("../../../src/heroes/pages/SearchPage");
 
 
 describe('Pruebas en <SearchPage />', () => {
@@ -13,7 +13,26 @@ describe('Pruebas en <SearchPage />', () => {
             </MemoryRouter>
         );
 
-        expect(container).toMatchInlineSnapshot(`<div />`);
+        expect(container).toMatchSnapshot();// borrar div
+
+    });
+
+    test('debe de mostrar a  Batman y el input con el valor del queryString', () => {
+
+        render(
+            <MemoryRouter initialEntries={['/search?q=batman']}>
+                <SearchPage />
+            </MemoryRouter>
+        );
+
+        const input = screen.getByRole('textbox');
+        expect( input.value ).toBe('batman');
+
+        const img = screen.getByRole('img');
+        expect( img.src ).toContain('/assets/heroes/dc-batman.jpg');
+
+        const alert = screen.getByLabelText('alert-danger');
+        expect( alert.style.display ).toBe('none');
 
     });
 
